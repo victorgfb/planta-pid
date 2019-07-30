978d157919e87f6b86d0e8f602de038db5534efe
Victor Gabriel <victorgfb80@gmail.com>
2019-07-30 17:41:34 -0300

    reorganiza o codigo

--- controle2.ino
+++ controle2.ino
@@ -6,10 +6,10 @@ char chr;
 void setup() {
   Serial.begin(9600);
   entrada = "0.7";
-  loop1();
+  enviaDados();
 }
 
-void loop1(){
+void enviaDados(){
    for(int i = 0; i < entrada.length(); i++){
       Serial.write(entrada[i]);
       delay(100);
@@ -17,34 +17,32 @@ void loop1(){
     Serial.write('*');
 }
 
-void loop2(){
+void esperaRecebimento(){
   while(1){
-       chr = (char) Serial.read();
-       if(chr == 'B'){
-          break; 
-       }
+      if(Serial.available()){
+         chr = (char) Serial.read();
+         if(chr == 'B'){
+            break; 
+         }
+      }
     }
 }
 
-void loop3(){
+void recebe(){
    entrada = "";
    while(1){
-      while(Serial.available()){
+      if(Serial.available()){
         chr = (char) Serial.read();
         if(chr == 'C')
           break;
         entrada.concat(chr);
       }
-
-      if(chr == 'C')
-          break;
    }
-
 }
 
 void loop() {
-  loop2();
-  loop3();
+  esperaRecebimento();
+  recebe();
   delay(1000);
-  loop1();
+  enviaDados();
 }
