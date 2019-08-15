28d194b9b2b7da778d08c2f971cccad240ef12a1
Victor Gabriel <victorgfb80@gmail.com>
2019-08-15 11:23:11 -0300

    organiza o codigo

--- draw.py
+++ draw.py
@@ -7,7 +7,12 @@ import csv
 style.use('fivethirtyeight')
 
 class anima:
- 
+    def __init__(self):
+        fig = plt.figure()
+        self.ax1 = fig.add_subplot(1,1,1)
+        self.ani = animation.FuncAnimation(fig, self.animate, interval = 500)
+        plt.show()
+    
     def animate(self, i):
         self.xs = np.array(0)
         self.ys = np.array(0)
@@ -20,15 +25,7 @@ class anima:
                 count += 1
                 self.ys = np.append(self.ys, float(row[0]))
 
-        ax1.clear()
-        ax1.plot(self.xs, self.ys)
-    
+        self.ax1.clear()
+        self.ax1.plot(self.xs, self.ys)
+        
 anim = anima()
-
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-
-ani = animation.FuncAnimation(fig, anim.animate, interval = 500)
-plt.show()
-
-
